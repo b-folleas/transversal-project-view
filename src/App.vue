f94c4a1e233485d1f3072aa2edc95529d37e9e6a
Eloi Bellet <eloi.bellet@cpe.fr>
2021-01-04 12:08:22 +0100

    remove home component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <Navbar/>
-  <router-view />
+  <div class="app-content">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
-import {Options, Vue} from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
 import Navbar from "./components/Navbar.vue";
 
 @Options({
   components: {
-    Navbar
-  }
+    Navbar,
+  },
 })
-export default class App extends Vue {
-}
+export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 @import "./assets/scss/bootstrap";
 
-html, body, #app {
+html,
+body,
+#app,
+.app-content {
   height: 100%;
-}
-
-#app {
-  margin-bottom: 0px;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "/map",
   },
   {
     path: "/truck",
